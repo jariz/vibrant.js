6bfd6bb24f41d7d9c595ad55d9ed39722b0dacc8
wilsaj <wilson.andrew.j+github@gmail.com>
2015-07-26 13:42:10 -0500

    put canvas cleanup in a try/finally block so there isn't a canvas element hanging around if something went wrong

--- dist/Vibrant.js
+++ dist/Vibrant.js
@@ -628,34 +628,37 @@ module.exports = MMCQ.quantize
         quality = 5;
       }
       image = new CanvasImage(sourceImage);
-      imageData = image.getImageData();
-      pixels = imageData.data;
-      pixelCount = image.getPixelCount();
-      allPixels = [];
-      i = 0;
-      while (i < pixelCount) {
-        offset = i * 4;
-        r = pixels[offset + 0];
-        g = pixels[offset + 1];
-        b = pixels[offset + 2];
-        a = pixels[offset + 3];
-        if (a >= 125) {
-          if (!(r > 250 && g > 250 && b > 250)) {
-            allPixels.push([r, g, b]);
+      try {
+        imageData = image.getImageData();
+        pixels = imageData.data;
+        pixelCount = image.getPixelCount();
+        allPixels = [];
+        i = 0;
+        while (i < pixelCount) {
+          offset = i * 4;
+          r = pixels[offset + 0];
+          g = pixels[offset + 1];
+          b = pixels[offset + 2];
+          a = pixels[offset + 3];
+          if (a >= 125) {
+            if (!(r > 250 && g > 250 && b > 250)) {
+              allPixels.push([r, g, b]);
+            }
           }
+          i = i + quality;
         }
-        i = i + quality;
+        cmap = this.quantize(allPixels, colorCount);
+        this._swatches = cmap.vboxes.map((function(_this) {
+          return function(vbox) {
+            return new Swatch(vbox.color, vbox.vbox.count());
+          };
+        })(this));
+        this.maxPopulation = this.findMaxPopulation;
+        this.generateVarationColors();
+        this.generateEmptySwatches();
+      } finally {
+        image.removeCanvas();
       }
-      cmap = this.quantize(allPixels, colorCount);
-      this._swatches = cmap.vboxes.map((function(_this) {
-        return function(vbox) {
-          return new Swatch(vbox.color, vbox.vbox.count());
-        };
-      })(this));
-      this.maxPopulation = this.findMaxPopulation;
-      this.generateVarationColors();
-      this.generateEmptySwatches();
-      image.removeCanvas();
     }
 
     Vibrant.prototype.generateVarationColors = function() {
